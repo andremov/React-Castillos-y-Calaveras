52fddafcce56aa295e869f4f37ab77cbd8833729
Andres-Mac <amovilla1103@gmail.com>
2020-01-12 00:06:14 -0500

    first public build

---
+++ cyc/build/precache-manifest.70ab8cf1affde5350465dba349b114e0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a570c12f0b1f863625aa7ed3c5fcfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12d4a5068c474523f278",
+    "url": "/static/css/main.94f5b37e.chunk.css"
+  },
+  {
+    "revision": "cd829dd181e6ffc2b744",
+    "url": "/static/js/2.e4e5076c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.e4e5076c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12d4a5068c474523f278",
+    "url": "/static/js/main.a0eeb680.chunk.js"
+  },
+  {
+    "revision": "1a4f3957704ac1e2f10b",
+    "url": "/static/js/runtime-main.be1aab8a.js"
+  },
+  {
+    "revision": "c93df555bb48774a4a0ddcf2ce7845b0",
+    "url": "/static/media/about.c93df555.svg"
+  },
+  {
+    "revision": "f71ed02dde6baf6a7d4b9c37fff5a6c3",
+    "url": "/static/media/cha.f71ed02d.svg"
+  },
+  {
+    "revision": "b17b56238c2933693b580d16a60c61d7",
+    "url": "/static/media/con.b17b5623.svg"
+  },
+  {
+    "revision": "49caa2de5bbd396f5db8865da69e0a55",
+    "url": "/static/media/d20.49caa2de.svg"
+  },
+  {
+    "revision": "d90a39cbe3e7629cea5c7f027f1f695b",
+    "url": "/static/media/dex.d90a39cb.svg"
+  },
+  {
+    "revision": "5767b0925ef9932aa73f4cf9747c3d96",
+    "url": "/static/media/int.5767b092.svg"
+  },
+  {
+    "revision": "a137c671b15ba597d9db5d1acb44b503",
+    "url": "/static/media/str.a137c671.svg"
+  },
+  {
+    "revision": "358055272ddaa26ac2c2c4375ff7ef30",
+    "url": "/static/media/wis.35805527.svg"
+  }
+]);
